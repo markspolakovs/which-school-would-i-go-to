de4f8d37524e79937c696f459cdb939f239d9d56
Marks Polakovs <github@markspolakovs.me>
2018-11-29 08:56:41 +0100

    fix for IE

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(function() {
     $(".loading-message").remove();
     $("#lookup").click(function() {
       const val = $("#postcode").val();
-      const entry = data.find(x => x.code_postal === val);
+      const entry = data.find(function(x) { return x.code_postal === val;});
       if (entry) {
         $("#result").text(
           "You would go to " + (entry.kirchberg ? "Kirchberg" : "Mamer")
